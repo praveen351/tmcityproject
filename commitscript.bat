ccfae012febbd06a83abcae042580d4ff9d81f93
praveen <praveenkumar1234raja@gmail.com>
2021-01-07 14:39:48 +0530

    commited

--- commitscript.bat
+++ commitscript.bat
@@ -1,5 +1,6 @@
+ECHO OFF
 git add .
 git commit -m "commited"
 git branch -M main
-git remote add origin https://github.com/praveen351/tmcityproject.git
+@git remote add origin https://github.com/praveen351/tmcityproject.git
 git push -u origin main
